903e7addd62248518bfe659787bc300a4e571e16
Marc Bastian Heinrichs <Mabahe@users.noreply.github.com>
2016-12-06 21:25:40 +0100

    simplify script

--- typo3-charset-converter.sh
+++ typo3-charset-converter.sh
@@ -12,12 +12,6 @@ TABLES="*"
 # Parse parameters, if any
 CONFIG=$1
 
-# Set this to 1 to change the table content and not only the scheme.
-# Otherwise, content is converted to binary in between which makes MySQL forget about conversion of data.
-# CONVERT_DATA=0: <source charset> => binary => UTF-8
-# CONVERT_DATA=1: <source charset> => UTF-8
-CONVERT_DATA=0
-
 # Wrapper for mysqldump
 do_mysqldump() {
 	TABLE=$1
@@ -71,39 +65,13 @@ for TABLE in $TABLES; do
 	# Make a dump of the schema
 	#echo -en "dumping schema... \t"
 	do_mysqldump $TABLE --no-data > ${TABLE}_schema.sql
-	# Make a dump of the whole tables (skip-extended-insert results in 1 line per record)
-	#echo -en "dumping data... \t"
-	do_mysqldump $TABLE --skip-extended-insert > ${TABLE}.sql
 
-	echo "done."
+	#echo "done."
 
 	echo -en "Convert table $TABLE... \t"
 
-	# First, convert all CHAR fields into VARCHAR due to an ugly bug(?) in MySQL which
-	# results in empty CHAR fields to become HEX(0000)
-	# (happens reproducibly in TYPO3 be_users.lang for example)
-	cat ${TABLE}_schema.sql | sed "s/,$//" | grep "^  " | egrep -v "KEY|FULLTEXT" | while read LINE; do
-		# Skip all lines except "char" fields
-		echo $LINE | grep -qi " char" || continue
-
-		# Convert the fields into varchar(). This will be changed back again to
-		# char() at the end of this script.
-		LINE=$(echo $LINE | sed "s/ char/ varchar/")
-
-		COLUMN=$(echo "$LINE" | awk '{print $1}')
-		#echo "Converting $COLUMN..."
-		do_mysql "ALTER TABLE $TABLE MODIFY $LINE;"
-	done
-
-	# Perform a two-step conversion via binary charset to avoid conversion of the data
-
-	if [ $CONVERT_DATA = 0 ]; then
-		#echo -en "to binary... \t"
-		do_mysql "ALTER TABLE $TABLE CONVERT TO CHARACTER SET binary;"
-	fi
-
 	#echo -en "to utf8... \t"
-	do_mysql "ALTER TABLE $TABLE CONVERT TO CHARACTER SET utf8;"
+	do_mysql "ALTER TABLE $TABLE DEFAULT CHARSET=utf8, COLLATE utf8_general_ci;"
 
 	echo "done."
 
@@ -115,7 +83,7 @@ for TABLE in $TABLES; do
 		LINE=$(echo "$LINE" | sed "s/ \(CHARACTER SET\|COLLATE\) [^ ]*//g")
 
 		COLUMN=$(echo "$LINE" | awk '{print $1}')
-		#echo "Converting $COLUMN..."
+		echo "Converting $COLUMN..."
 		do_mysql "ALTER TABLE $TABLE MODIFY $LINE;"
 	done
 done
